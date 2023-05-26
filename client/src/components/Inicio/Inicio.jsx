64287f526c526991d14cebe07d8712376b76d9e2
Juan Esteban <juancho50301@gmail.com>
2023-05-26 09:35:37 -0500

    'diseños'

--- client/src/components/Inicio/Inicio.jsx
+++ client/src/components/Inicio/Inicio.jsx
@@ -7,12 +7,13 @@ const Inicio = () => {
     return (
         <div className='inicio'>
             <h1 className='titulo-inicio'>App VideoJuegos</h1>
-            <img 
+            <div className='animacion-container'>
+                 <img 
             className='imagen-inicio'
-            src="https://w0.peakpx.com/wallpaper/592/429/HD-wallpaper-god-of-war-izaan.jpg" 
+            src="https://www.eltiempo.com/files/article_main_1200/uploads/2023/04/12/6436dbfe1f6ed.png" 
             alt="imagen" 
             />
-            <div></div>
+            </div>
             <Link to={`/home`}>
             <button className='ingresar'>Ingresar</button>
             </Link>
