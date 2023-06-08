530fa02a0ca47d324e5e78113c1ec16d5d394963
Juan Esteban <juancho50301@gmail.com>
2023-06-08 18:56:19 -0500

    'mejoras'

--- api/src/controllers/getGames.js
+++ api/src/controllers/getGames.js
@@ -4,31 +4,41 @@ const { URL, API_KEY } = process.env;
 
 
 
-const getGames = async() => {
-
-    try{
-        const response = await axios.get(`${URL}?key=${API_KEY}`,{timeout: 5000});
-        const data = response.data;
-        //console.log(data.results)
-        const games = data.results.map(game=>{
-            return {
-                id: game.id,
-                name: game.name,
-                released: game.released,
-                rating: game.rating,
-                platforms: game.platforms.map(pl=>pl.platform.name),
-                // image : game.platforms.map(pl=>pl.platform.image_background),
-                image: game.background_image,
-                genres : game.genres.map(gen=>gen.slug)
-            }
-        });
-        return games;
-        //return data.results;
-    }catch(error){
-        console.log(error);
-        return error;
+const getGames = async () => {
+    try {
+        //como son 5 urls y cada una me trae 20 juegos y necesito 100 resuelvo lo siguiente...
+      const games = [];
+      const totalPages = 5;
+      const requests = [];
+  
+      for (let page = 1; page <= totalPages; page++) {
+        requests.push(axios.get(`${URL}?key=${API_KEY}&page=${page}`, { timeout: 5000 }));
+      }
+  
+      const responses = await Promise.all(requests);
+  
+      for (const response of responses) {
+        const data = response.data.results;
+        games.push(
+          ...data.map((game) => ({
+            id: game.id,
+            name: game.name,
+            released: game.released,
+            rating: game.rating,
+            platforms: game.platforms.map((pl) => pl.platform.name),
+            image: game.background_image,
+            genres: game.genres.map((gen) => gen.slug),
+          }))
+        );
+      }
+  
+      console.log('myList ->', games.length);
+      return games;
+    } catch (error) {
+      console.log(error);
+      return error;
     }
-
-};
+  };
+  
 
 module.exports = {getGames};
--- client/src/components/gamesContainer/pagination/pagination.jsx
+++ client/src/components/gamesContainer/pagination/pagination.jsx
@@ -3,30 +3,56 @@ import './Pagination.css'
 
 
 const Pagination = ({ itemsPerPage, totalItems, paginate, currentPage }) => {
-    const pageNumbers = [];
-  
-    for (let i = 1; i <= Math.ceil(totalItems / itemsPerPage); i++) {
-      pageNumbers.push(i);
+  const totalPages = Math.ceil(totalItems / itemsPerPage);
+  const maxButtons = 10;
+  let startPage, endPage;
+
+  if (totalPages <= maxButtons) {
+    // Mostrar todos los números de página si hay menos de 10 páginas en total
+    startPage = 1;
+    endPage = totalPages;
+  } else {
+    // Calcular los números de página para mostrar alrededor de la página actual
+    const maxVisiblePages = maxButtons - 2; // Restar 2 para los botones "Prev" y "Next"
+    const maxPagesBeforeCurrent = Math.floor(maxVisiblePages / 2);
+    const maxPagesAfterCurrent = Math.ceil(maxVisiblePages / 2) - 1;
+
+    if (currentPage <= maxPagesBeforeCurrent) {
+      // Estamos en las primeras páginas
+      startPage = 1;
+      endPage = maxVisiblePages;
+    } else if (currentPage + maxPagesAfterCurrent >= totalPages) {
+      // Estamos en las últimas páginas
+      startPage = totalPages - maxVisiblePages + 1;
+      endPage = totalPages;
+    } else {
+      // Estamos en el medio de las páginas
+      startPage = currentPage - maxPagesBeforeCurrent;
+      endPage = currentPage + maxPagesAfterCurrent;
     }
-  
-    return (
-      <nav>
-        <ul className="pagination">
-          <li className={currentPage === 1 ? "disabled" : ""}>
-            <button onClick={() => paginate(currentPage - 1)}>Prev</button>
+  }
+
+  const pageNumbers = [...Array(endPage - startPage + 1).keys()].map((i) => startPage + i);
+
+  return (
+    <nav>
+      <ul className="pagination">
+        <li className={currentPage === 1 ? "disabled" : ""}>
+          <button onClick={() => paginate(currentPage - 1)}>Prev</button>
+        </li>
+        {pageNumbers.map((number) => (
+          <li key={number} className={currentPage === number ? "active" : ""}>
+            <button onClick={() => paginate(number)}>{number}</button>
           </li>
-          {pageNumbers.map((number) => (
-            <li key={number} className={currentPage === number ? "active" : ""}>
-              <button onClick={() => paginate(number)}>{number}</button>
-            </li>
-          ))}
-          <li className={currentPage === pageNumbers.length ? "disabled" : ""}>
-            <button onClick={() => paginate(currentPage + 1)}>Next</button>
-          </li>
-        </ul>
-      </nav>
-    );
-  };
+        ))}
+        <li className={currentPage === totalPages ? "disabled" : ""}>
+          <button onClick={() => paginate(currentPage + 1)}>Next</button>
+        </li>
+      </ul>
+    </nav>
+  );
+};
+
       
     
     
